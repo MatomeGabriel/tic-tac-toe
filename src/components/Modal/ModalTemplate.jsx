8d4c17d595c8c90b6592c35a3311aad8460d3aaa
Gabriel <montjagabriel@gmail.com>
2025-06-17 21:19:04 +0200

    fixed icon-o-outline.svg name

--- src/components/Modal/ModalTemplate.jsx
+++ src/components/Modal/ModalTemplate.jsx
@@ -1,4 +1,3 @@
-
 import { useContext } from "react";
 
 import { createPortal } from "react-dom";
--- src/pages/Game/GameCell/GameCell.jsx
+++ src/pages/Game/GameCell/GameCell.jsx
@@ -4,11 +4,10 @@ import { GameContext } from "../../../contexts/GameContext";
 import IconX from "../../../assets/icon-x.svg?react";
 import IconO from "../../../assets/icon-o.svg?react";
 import IconXOutline from "../../../assets/icon-x-outline.svg?react";
-import IconOOutline from "../../../assets/icon-O-outline.svg?react";
+import IconOOutline from "../../../assets/icon-o-outline.svg?react";
 import { ModalContext } from "../../../contexts/ModalContext";
 import RoundOverModal from "../../../components/Modals/RoundOverModals/RoundOverModal";
 import { SfxContext } from "../../../contexts/SfxContext";
-
 
 const GameCell = ({ cellItem, index }) => {
   const { updateBoard, game, checkForWinner, roundComplete } =
@@ -18,7 +17,6 @@ const GameCell = ({ cellItem, index }) => {
     useContext(SfxContext);
   const { handleModal } = useContext(ModalContext);
   const cellClickHandler = () => {
-
     // this index is retained by the function scope, this happens because of something known as closure.
     updateBoard(index);
     const result = checkForWinner(game.board);
